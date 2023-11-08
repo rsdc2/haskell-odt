144bf96de74f8b751e5555ff9ec2198390181b42
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-11-08 20:05:45 +0000

    added getSpans

--- src/Text/ODT/Query.hs
+++ src/Text/ODT/Query.hs
@@ -5,10 +5,19 @@ module Text.ODT.Query where
 
 import Text.ODT.ODT
 
+
 getParas :: ODT -> [ODT]
 getParas odt 
     | ODTSeq odt1 odt2 <- odt = getParas odt1 <> getParas odt2
     | TextNode (P pstyle) odtxml children <- odt = [TextNode (P pstyle) odtxml children]
     | TextNode _ _ _ <- odt = []
     | OfficeNode typ odtxml children <- odt = getParas children
+    | otherwise = []
+
+getSpans :: ODT -> [ODT]
+getSpans odt 
+    | ODTSeq odt1 odt2 <- odt = getSpans odt1 <> getSpans odt2
+    | TextNode (Span tstyle) odtxml children <- odt = [TextNode (T tstyle) odtxml children]
+    | TextNode _ _ _ <- odt = []
+    | OfficeNode typ odtxml children <- odt = getSpans children
     | otherwise = []
