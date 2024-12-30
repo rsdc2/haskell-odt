bcec6ecb8dea756a29acb64d2e0adfb09e0021df
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-12-30 21:04:31 +0000

    Add better type aliases

--- src/Text/ODT/Zip/Utils.hs
+++ src/Text/ODT/Zip/Utils.hs
@@ -5,22 +5,23 @@ import System.Directory (createDirectoryIfMissing)
 import qualified Text.ODT.Zip.Zip as Zip
 import Text.ODT.XML.Prettify ( prettifyFile )
 
-type Folderpath = String
+type SrcFolderPath = String
+type DstFolderPath = String
 type Filename = String
-type Filepath = String
 
 -- Unzip odt file and write prettified xml files to dstpath
 -- param odtpath: folderpath containing .odt file
 -- param filename: filename of .odt file without .odt extension
 -- param dstpath: destination folder of folder containing archive to be unzipped
-unzipFiles :: Folderpath -> Filename -> Folderpath -> IO ()
-unzipFiles odtpath filename dstpath = do
-    Zip.unzip (odtpath <> "/" <> filename <> ".odt") (dstpath <> "/" <> filename)
+unzipFilesAndPrettify :: SrcFolderPath -> Filename -> DstFolderPath -> IO ()
+unzipFilesAndPrettify srcpath filename dstpath = do
+    Zip.unzip (srcpath <> "/" <> filename <> ".odt") (dstpath <> "/" <> filename)
     prettifyFile (dstpath <> "/" <> filename <> "/styles.xml") (dstpath <> "/prettified/styles.xml")
     prettifyFile (dstpath <> "/" <> filename <> "/content.xml") (dstpath <> "/prettified/content.xml")
 
--- 
-prettifyODT :: Folderpath -> Filename -> Folderpath -> IO ()
-prettifyODT srcpath filename dstpath = do
-    
+-- -- 
+-- prettifyODT :: Folderpath -> Filename -> Folderpath -> IO ()
+-- prettifyODT srcpath filename dstpath = do
+
+
     
