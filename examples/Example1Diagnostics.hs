d1d5604f2a4298a7797536c15e26ab715dae96d4
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-02 17:12:36 +0000

    Change to default paragraph format

--- examples/Example1Diagnostics.hs
+++ examples/Example1Diagnostics.hs
@@ -9,7 +9,7 @@ main = do
     archive <- archiveFromZip "./examples" fn "./.working"
     prettifyODT "./.working" fn
 
-    let contentODT = getContentDocODT archive <> para Nothing "" <> textspan Nothing "hello" <> para Nothing "" <> textspan Nothing "goodbye"
+    let contentODT = getContentDocODT archive <> para normalPara "" <> textspan Nothing "hello" <> para Nothing "" <> textspan Nothing "goodbye"
     let archive' = replaceContentDocODT contentODT archive 
     let options = defaultODTFileOptions { workingFolder = Just "./.working", removeWorkingFolder = False, removeWorkingPath = False } 
     updateODTFile archive' "./examples" fn "./examples/output" fn options
