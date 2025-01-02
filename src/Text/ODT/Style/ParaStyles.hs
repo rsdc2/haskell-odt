3092210f67accf7cc1882d43deb38e8c455e6a1d
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-02 14:45:47 +0000

    Add normalPara paragraph style

---
+++ src/Text/ODT/Style/ParaStyles.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.ODT.Style.ParaStyles (
+    normalPara
+) 
+
+where
+
+import Text.ODT.Style.Types
+
+normalPara :: ParaStyle
+normalPara = newParaStyle
