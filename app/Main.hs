426457b12e5d9595070f49c15b7050d12151de79
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 17:58:16 +0000

    Experiment with setting up styles

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,6 @@ setupStyles = do
 
 minimalODT :: Writer ODT ()
 minimalODT = do
-    italicParaM
     textspanM italic "Hello"
     textspanM bold " world."
     paraM normalPara ""
@@ -19,4 +18,5 @@ minimalODT = do
     paraM italicPara "This text is italic because it is in an italic paragraph."
 
 main :: IO ()
-main = saveNewODTDiag "./examples/output" "newodt" minimalODT
+main = do
+    saveNewODTWithStylesDiag "./examples/output" "newodt" minimalODT setupStyles
