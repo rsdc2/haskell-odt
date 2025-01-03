f83b5a57eec7a3b636bd2805016714f975719688
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 15:02:11 +0000

    Move key functionality to specialised files

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Text.ODT
-import Text.ODT.Diagnostics.Utils
+import Text.ODT.Diagnostics
 
 import Control.Monad.Writer
 
-minimalExampleWithDiagnostics :: IO () 
-minimalExampleWithDiagnostics = do
-    let fn = "empty"
-    archive <- archiveFromZip "./examples" fn "./.working"
-    prettifyODT "./.working" fn
-
-    let contentODT = getContentDocODT archive <> textspan (Just bold) "hello"
-
-    putStrLn . show $ contentODT    
-    let archive' = replaceContentDocODT contentODT archive 
-    let options = defaultODTFileOptions { workingFolder = Just "./.working", removeWorkingFolder = False, removeWorkingPath = False } 
-    updateODTFile archive' "./examples" fn "./examples/output" fn options
-    prettifyODT "./.working" fn
-    return ()
-
-minimalODTWriter :: Writer ODT ()
-minimalODTWriter = do
-    textspanM Nothing "hello"
-    textspanM Nothing "goodbye"
-    return ()
+minimalODT :: Writer ODT ()
+minimalODT = do
+    textspanM (Just italic) "Hello"
+    textspanM (Just bold) " world."
 
 main :: IO ()
-main = do
-    let fn = "empty"
-    archive <- archiveFromZip "./examples" fn "./.working"
-    prettifyODT "./.working" fn
-
-    let contentODT = getContentDocODT archive <> (execWriter minimalODTWriter) 
-
-    putStrLn . show $ contentODT    
-    let archive' = replaceContentDocODT contentODT archive 
-    let options = defaultODTFileOptions { workingFolder = Just "./.working", removeWorkingFolder = False, removeWorkingPath = False } 
-    updateODTFile archive' "./examples" fn "./examples/output" fn options
-    prettifyODT "./.working" fn
-    return ()
- 
+main = saveNewODTDiag "./examples/output" "newodt" minimalODT
