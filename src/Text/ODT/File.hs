2cf282557192cf2775cc583de108d44e88fab90d
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 15:01:11 +0000

    Add saveNewODT and appendToODT functions

--- src/Text/ODT/File.hs
+++ src/Text/ODT/File.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Text.ODT.File ( concatPath, concatFileExt ) where
+module Text.ODT.File ( 
+      concatPath
+    , concatFileExt
+    , appendToODT
+    , saveNewODT
+    , Filename
+    , Folderpath
+    , templatesPath ) where
 
-import Data.List ( intersperse, intercalate )
+import Data.List ( intercalate )
+import Control.Monad.Writer
+
+import Text.ODT.ODT
+import Text.ODT.Extract
+import Text.ODT.Compress
 
 type Filename = String
 type Rootpath = String
@@ -10,10 +22,31 @@ type Filepath = String
 type Folderpath = String
 type Ext = String
 
-
+templatesPath :: Folderpath
+templatesPath = "./templates"
 
 concatFileExt :: Filename -> Ext -> Filepath
 concatFileExt fn ext = fn <> "." <> ext
 
 concatPath :: Rootpath -> [Folderpath] -> Filename -> Ext -> Filepath
 concatPath fp1 fps fn ext = intercalate "/" ([fp1] <> fps) <> "/" <> fn <> "." <> ext
+
+saveNewODT :: Folderpath -> Filename -> Writer ODT () -> IO ()
+saveNewODT fp fn odt = do
+    archive <- archiveFromZip templatesPath "empty" "./.working"
+
+    let contentODT = getContentDocODT archive <> execWriter odt
+
+    let archive' = replaceContentDocODT contentODT archive 
+    let options = defaultODTFileOptions { workingFolder = Just "./.working", removeWorkingFolder = True, removeWorkingPath = True } 
+    updateODTFile archive' templatesPath "empty" fp fn options
+
+appendToODT :: Folderpath -> Filename -> Writer ODT () -> IO ()
+appendToODT fp fn odt = do
+    archive <- archiveFromZip fp fn "./.working"
+
+    let contentODT = getContentDocODT archive <> execWriter odt
+
+    let archive' = replaceContentDocODT contentODT archive 
+    let options = defaultODTFileOptions { workingFolder = Just "./.working", removeWorkingFolder = True, removeWorkingPath = True } 
+    updateODTFile archive' fp fn fp (fn <> "_modified") options
