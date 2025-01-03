34c9875e167d9ac933a7558fb53f9ed5086ed95b
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 10:39:34 +0000

    Implement example with Writer Monad

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,36 @@
 import Text.ODT
 import Text.ODT.Diagnostics.Utils
 
+import Control.Monad.Writer
+
+minimalExampleWithDiagnostics :: IO () 
+minimalExampleWithDiagnostics = do
+    let fn = "empty"
+    archive <- archiveFromZip "./examples" fn "./.working"
+    prettifyODT "./.working" fn
+
+    let contentODT = getContentDocODT archive <> textspan (Just bold) "hello"
+
+    putStrLn . show $ contentODT    
+    let archive' = replaceContentDocODT contentODT archive 
+    let options = defaultODTFileOptions { workingFolder = Just "./.working", removeWorkingFolder = False, removeWorkingPath = False } 
+    updateODTFile archive' "./examples" fn "./examples/output" fn options
+    prettifyODT "./.working" fn
+    return ()
+
+minimalODTWriter :: Writer ODT ()
+minimalODTWriter = do
+    textspanM Nothing "hello"
+    textspanM Nothing "goodbye"
+    return ()
+
 main :: IO ()
 main = do
     let fn = "empty"
     archive <- archiveFromZip "./examples" fn "./.working"
     prettifyODT "./.working" fn
 
-    let contentODT = getContentDocODT archive <> textspan (Just bold) "hello"
+    let contentODT = getContentDocODT archive <> (execWriter minimalODTWriter) 
 
     putStrLn . show $ contentODT    
     let archive' = replaceContentDocODT contentODT archive 
