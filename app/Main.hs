dc3ea6165e95a58f9cee775a8ebe33e42141a260
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 17:23:40 +0000

    Add more formatting and content

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,18 @@ import Text.ODT.Diagnostics
 
 import Control.Monad.Writer
 
+setupStyles :: Writer ODT ()
+setupStyles = do
+    italicParaM
+
 minimalODT :: Writer ODT ()
 minimalODT = do
-    textspanM (Just italic) "Hello"
-    textspanM (Just bold) " world."
+    italicParaM
+    textspanM italic "Hello"
+    textspanM bold " world."
+    paraM normalPara ""
+    textspanM underline "This text is underlined."
+    paraM italicPara "This text is italic because it is in an italic paragraph."
 
 main :: IO ()
-main = appendToODTDiag "./examples/output" "newodt" minimalODT
+main = saveNewODTDiag "./examples/output" "newodt" minimalODT
