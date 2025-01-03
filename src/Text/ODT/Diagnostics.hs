89cd7ebf6b1b5fbccb79b8b7fface8780e18fd70
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 17:57:58 +0000

    Reexport new functions for appending styles

--- src/Text/ODT/Diagnostics.hs
+++ src/Text/ODT/Diagnostics.hs
@@ -1,4 +1,4 @@
-module Text.ODT.Diagnostics (prettifyODT, saveNewODTDiag, appendToODTDiag) where
+module Text.ODT.Diagnostics (prettifyODT, saveNewODTDiag, appendToODTDiag, saveNewODTWithStylesDiag, saveNewStylesDiag) where
 
 import Text.ODT.Diagnostics.File
 import Text.ODT.Diagnostics.Utils
