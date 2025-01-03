3026a0138b8ae4b80f0ea6c3f86cd70cd34a8e34
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-03 17:21:59 +0000

    Add Writer monad variants of paragraph styles

--- src/Text/ODT/Style/ParaStyles.hs
+++ src/Text/ODT/Style/ParaStyles.hs
@@ -1,12 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Text.ODT.Style.ParaStyles (
-    normalPara
+      normalPara
+    , italicPara
+    , italicParaM
 ) 
 
 where
 
+import Control.Monad.Writer
 import Text.ODT.Style.Types
+import Text.ODT.ODT
 
 normalPara :: ParaStyle
 normalPara = newParaStyle
+
+italicPara :: ParaStyle
+italicPara = newParaStyle {paraTextProps = newTextProps {fontStyle = Italic}, paraStyleName = Just "italicPara"}
+
+italicParaM :: Writer ODT ()
+italicParaM = tell . toODT $ italicPara
